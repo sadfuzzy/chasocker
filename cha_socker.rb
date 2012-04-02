de33a236253d4995a0a1427b40ca059bb2d155d6
Denis Savitsky <sadfuzzy@yandex.ru>
2012-04-02 14:57:21 +0400

    Defaults.

--- cha_socker.rb
+++ cha_socker.rb
@@ -110,18 +110,22 @@ class ChaSocker < GServer
   end
 end
 
+host, port = "localhost", 46969
+
 # Show Help page if ARGV includes "-h" option
 if ARGV.include?("-h")
-  $stderr.puts "Usage: #{$0} host:port\n\tDefault settings: localhost:46969"
+
+  $stderr.puts "Usage: #{$0} host:port\n\tDefault settings: #{host}:#{port}"
   exit 1
+
 else
+
   # Customized start
   if ARGV[0]
+
     args = ARGV[0].split(':')
     host, port = args[0], args[1]
-  else
-    # Start without params
-    host, port = "localhost", 46969
+
   end
 end
 
