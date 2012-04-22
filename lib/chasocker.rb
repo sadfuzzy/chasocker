5a4a7b7d17bc1b9af4a45d4ae06ed8ebde363ad7
Denis Savitsky <sadfuzzy@yandex.ru>
2012-04-22 20:24:49 +0400

    1st test.

--- lib/chasocker.rb
+++ lib/chasocker.rb
@@ -1,5 +1,33 @@
 require "chasocker/version"
+require "gserver"
 
 module Chasocker
-  # Your code goes here...
+  class Server < GServer
+
+    def initialize(*args)
+
+      super(*args)
+
+      #Keep a list for broadcasting messages
+      @chatters = { }
+
+      # We'll need this for thread safety
+      @mutex = Mutex.new
+
+    end
+
+    def chatters
+
+      @chatters
+
+    end
+
+    def serve(io)
+
+      io.puts("LOGIN")
+
+      self.chatters[1] = "some"
+
+    end
+  end
 end
--- spec/chasocker/chasocker_spec.rb
+++ spec/chasocker/chasocker_spec.rb
@@ -1,8 +1,39 @@
-require "rspec"
+require "spec_helper"
 
 describe Chasocker do
-  it "should do something" do
+  before(:all) do
 
-    true.should == true
+    HOST, PORT, max = '127.0.0.1', 46969, 4
+
+    @server = Chasocker::Server.new(PORT, HOST, max, $stderr, false, true)
+    @server.start
+
   end
+
+  it "should properly accept connections" do
+
+    @server.connections.should == 0
+    @server.chatters.size.should == 0
+
+    socket = TCPSocket.new(HOST, PORT)
+
+    socket.readline.chomp.should == "LOGIN"
+    #@server.connections.should == 1
+    @server.chatters.size.should == 1
+
+    socket.close
+
+    sleep 1
+
+    @server.connections.should == 0
+    @server.chatters.size.should == 1
+
+  end
+
+  after(:all) do
+
+    @server.shutdown
+
+  end
+
 end
